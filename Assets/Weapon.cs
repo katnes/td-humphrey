8dbc71f55311bb7d1a20074e8f57fd12712ab574
Kat Powell <thekatnes@gmail.com>
2021-11-24 21:38:40 -0600

    Flames shoot from torch!!
    PEW PEW!

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameObject projectile;
-    public Transform shotPoint;
+    public GameObject shotPoint;
     public float timeBetweenShots;
 
     private float shotTime;
 
     private void Update()
     {
-        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; 
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        
+        Vector2 direction = worldPoint - transform.position;
+
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         transform.rotation = rotation;
 
         if(Input.GetMouseButton(0)) {
-            if (Time.time >= shotTime){
-                Instantiate(projectile, shotPoint.position, transform.rotation);
-                shotTime = Time.time + timeBetweenShots;
+            Debug.Log("The Left Click Happened!");
+            Debug.LogFormat ("Mouse Position is: {0}, {1}", Input.mousePosition.x, Input.mousePosition.y);
+            Debug.LogFormat("Worldpoint: {0}, {1}", worldPoint.x, worldPoint.y);
+            Debug.LogFormat ("direction", direction);
+            Debug.LogFormat ("Transform position is: {0}, {1}", transform.position.x, transform.position.y);
+            Debug.Log(direction);
+            if (shotTime >= 0.1){
+                Debug.Log("Raining Hell Down Upon My Enemies...");
+                // Instantiate(projectile, shotPoint.position, transform.rotation);
+                var newProjectile = Instantiate(projectile);
+                // newProjectile.transform.position = new Vector3(-1.583f, -2.873f + 5, 0);
+                newProjectile.transform.position = shotPoint.transform.position;
+                newProjectile.transform.rotation = transform.rotation;
+                shotTime = 0;
             }
         }
+         shotTime += Time.deltaTime;
     }
 }
