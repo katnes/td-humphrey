022c0031d2f6493074845052456a45cbdda2482d
Kat Powell <thekatnes@gmail.com>
2021-11-24 19:04:18 -0600

    torch & projectiles
    Added new assets

---
+++ Assets/Projectile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+   public float speed;
+   public float lifeTime; 
+
+    private void Start(){
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update(){
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+}
